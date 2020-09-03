c74f4ba1153b7847fbe797fb6eeb8bee4122097b
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-03 15:20:22 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchase
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/i }, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date, presence: true
+  validates :nickname,              presence: true, format: { message: "入力が正しくありません。"}
+  validates :email,                 presence: true, uniqueness: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "入力が正しくありません。" }
+  validates :password,              presence: true, format: { with: /\A[a-z0-9]+\z/i, message: "入力が正しくありません。" }, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, format: { message: "入力が正しくありません。" }
+  validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "入力が正しくありません。" }
+  validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "入力が正しくありません。" }
+  validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "入力が正しくありません。" }
+  validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "入力が正しくありません。" }
+  validates :birth_date,            presence: true, format: { message: "入力が正しくありません。" }
 end
