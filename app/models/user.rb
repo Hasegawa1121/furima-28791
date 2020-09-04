af28d3c27192d00d7db961a309e315af8964ca27
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-04 18:07:40 +0900

    ユーザー管理機能の実装、viewファイル入れ替え、テストコードの準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :purchase
+  has_many :purchases
 
-  validates :nickname,              presence: true, format: { message: "入力が正しくありません。"}
+  validates :nickname,              presence: { message: "入力が正しくありません。" }
   validates :email,                 presence: true, uniqueness: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "入力が正しくありません。" }
   validates :password,              presence: true, format: { with: /\A[a-z0-9]+\z/i, message: "入力が正しくありません。" }, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, format: { message: "入力が正しくありません。" }
+  validates :password_confirmation, presence: { message: "入力が正しくありません。" }
   validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "入力が正しくありません。" }
   validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "入力が正しくありません。" }
   validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "入力が正しくありません。" }
   validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "入力が正しくありません。" }
-  validates :birth_date,            presence: true, format: { message: "入力が正しくありません。" }
+  validates :birth_date,            presence: { message: "入力が正しくありません。" }
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it "nicknameは必須" do
+    end
+    it "メールアドレスは必須" do
+    end
+    it "メールアドレスが一意性である事" do
+    end
+    it "メールアドレスは@を含む" do
+    end
+    it "パスワードは必須" do
+    end
+    it "パスワードは6文字以上" do
+    end
+    it "パスワードが半角英数混合" do
+    end
+    it "パスワードが確認用と一致する" do
+    end
+
+  end
+
+
+end
