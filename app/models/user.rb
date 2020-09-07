485b5906d069eb12a7e456a9734594b09ec17986
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-07 17:23:17 +0900

    正規表現の変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,18 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  NAME_REGEX_KANA = /\A[ァ-ヶー－]+\z/
+
   with_options presence: { message: '入力が正しくありません。' } do
     validates :nickname
     validates :email,                 uniqueness: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     validates :password,              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }, length: { minimum: 6 }
     validates :password_confirmation
-    validates :last_name,             format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name,            format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name_kana,        format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana,       format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name,             format: { with: NAME_REGEX }
+    validates :first_name,            format: { with: NAME_REGEX }
+    validates :last_name_kana,        format: { with: NAME_REGEX_KANA }
+    validates :first_name_kana,       format: { with: NAME_REGEX_KANA }
     validates :birth_date
   end
 end
