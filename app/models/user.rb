b0c2e22b4afd1edf270f2c8761edcbf7c0d643a2
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-07 12:01:08 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,13 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  validates :nickname,              presence: { message: "入力が正しくありません。" }
-  validates :email,                 presence: true, uniqueness: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "入力が正しくありません。" }
-  validates :password,              presence: true, format: { with:  /\A(?=.*?[a-z])[a-z\d]+\z/, message: "入力が正しくありません。" }, length: { minimum: 6 }
-  validates :password_confirmation, presence: { message: "入力が正しくありません。" }
-  validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "入力が正しくありません。" }
-  validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "入力が正しくありません。" }
-  validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "入力が正しくありません。" }
-  validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "入力が正しくありません。" }
-  validates :birth_date,            presence: { message: "入力が正しくありません。" }
-
+  validates :nickname,              presence: { message: '入力が正しくありません。' }
+  validates :email,                 presence: true, uniqueness: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: '入力が正しくありません。' }
+  validates :password,              presence: true, format: { with: /\A(?=.*?[a-z])[a-z\d]+\z/, message: '入力が正しくありません。' }, length: { minimum: 6 }
+  validates :password_confirmation, presence: { message: '入力が正しくありません。' }
+  validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '入力が正しくありません。' }
+  validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '入力が正しくありません。' }
+  validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '入力が正しくありません。' }
+  validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '入力が正しくありません。' }
+  validates :birth_date,            presence: { message: '入力が正しくありません。' }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,57 +6,54 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it "nicknameは必須" do
-      @user.nickname = ""
+    it 'nicknameは必須' do
+      @user.nickname = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname 入力が正しくありません。")
+      expect(@user.errors.full_messages).to include('Nickname 入力が正しくありません。')
     end
-    it "メールアドレスは必須" do
-      @user.email = ""
+    it 'メールアドレスは必須' do
+      @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
-    it "メールアドレスが一意性である事" do
+    it 'メールアドレスが一意性である事' do
       @user.save
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include("Email has already been taken")
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
-    it "メールアドレスは@を含む" do
-      @user.email = "aaaabbbb"
+    it 'メールアドレスは@を含む' do
+      @user.email = 'aaaabbbb'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email is invalid")
+      expect(@user.errors.full_messages).to include('Email is invalid')
     end
-    it "パスワードは必須" do
-      @user.password = ""
+    it 'パスワードは必須' do
+      @user.password = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
-    it "パスワードは6文字以上" do
-      @user.password = "ps000"
-      @user.password_confirmation = "ps000"
+    it 'パスワードは6文字以上' do
+      @user.password = 'ps000'
+      @user.password_confirmation = 'ps000'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
-    it "パスワードが半角英数混合" do
-      @user.password = "12345678"
-      @user.password_confirmation = "12345678"
+    it 'パスワードが半角英数混合' do
+      @user.password = '12345678'
+      @user.password_confirmation = '12345678'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password 入力が正しくありません。")
+      expect(@user.errors.full_messages).to include('Password 入力が正しくありません。')
     end
-    it "確認用パスワードも入力されている" do
-      @user.password_confirmation = ""
+    it '確認用パスワードも入力されている' do
+      @user.password_confirmation = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation 入力が正しくありません。")
+      expect(@user.errors.full_messages).to include('Password confirmation 入力が正しくありません。')
     end
-    it "生年月日が必須" do
-      @user.birth_date = ""
+    it '生年月日が必須' do
+      @user.birth_date = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Birth date 入力が正しくありません。")
+      expect(@user.errors.full_messages).to include('Birth date 入力が正しくありません。')
     end
-
   end
-
-
 end
