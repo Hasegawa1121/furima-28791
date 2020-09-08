7c2786bcde3cf665d8c8a149245c3247eba94396
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-08 15:21:42 +0900

    ActiveHashの導入・出品商品の詳細の設定

--- db/migrate/20200908040555_create_items.rb
+++ db/migrate/20200908040555_create_items.rb
@@ -1,16 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :name
-      t.text :description
-      t.string :image
-      t.integer :category_id
-      t.integer :condition_id
-      t.integer :postage_payer_id
-      t.integer :prefecture_id
-      t.integer :handling_id
-      t.integer :price
+      t.references :user,              null: false, foreign_key: true
+      t.string     :name,              null: false
+      t.text       :description,       null: false
+      t.string     :image,             null: false
+      t.integer    :category_id,       null: false
+      t.integer    :condition_id,      null: false
+      t.integer    :postage_payer_id,  null: false
+      t.integer    :prefecture_id,     null: false
+      t.integer    :handling_id,       null: false
+      t.integer    :price,             null: false
 
       t.timestamps
     end
