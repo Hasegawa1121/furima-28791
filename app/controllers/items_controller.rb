6c56f04306f166f1bd72419f9342edfba04bd429
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-08 13:11:50 +0900

    Revert "Revert "ActiveStorageの導入""
    This reverts commit 6003c6fd42399f17e081aca35a126c655cb1c087.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,13 @@ class ItemsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :category_id, :condition_id, :postage_payer_id, :prefecture_id_id, :handling_time_id).merge(user_id: current_user.id)
+  end
+
 end
