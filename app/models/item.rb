9af005922686c8d0acbb86f880baf442b8a8dfc5
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-09 15:06:20 +0900

    validationの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :handling
   belongs_to_active_hash :postage_payer
   belongs_to_active_hash :prefecture
+
+  with_options presence: {message: '入力が正しくありません。'} do
+    validates :name,            length: { maximum: 40 }
+    validates :description,     length: { maximum: 1000 }
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :handling_id
+    validates :price,           length: { minimum: 300 }, length: { maximum: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
 end
