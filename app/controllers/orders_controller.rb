d29f6de30369fb0aa543e466110976a66ca73f41
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-14 16:55:57 +0900

    モデル作成・コントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @order = Order.new
   end
 
   def create
+    @item = Item.find(params[:id])
     @order = Order.new(order_params)
     if @order.valid?
       pay_item
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  has_one :address
+
   attr_accessor :token
-  validates :price, presence: true
   validates :token, presence: true
 end
