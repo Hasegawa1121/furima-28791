21969d95b79adbf67b683444bdd9b47e44c6e9d5
Hasegawa1121 <69379032+Hasegawa1121@users.noreply.github.com>
2020-09-14 21:00:11 +0900

    addressesのバリデーション追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  with_options presence: true do
+    validates :postal_code,   format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :city
+    validates :addresses
+    validates :phone_number,  format: { with: /\A\d{10,11}\z/ }
+  end
+  validates :building
+
 end
--- db/migrate/20200914034651_create_addresses.rb
+++ db/migrate/20200914034651_create_addresses.rb
@@ -1,12 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.string :postal_code, null: false
-      t.integer :prefecture_id, null: false
-      t.string :city, null: false
-      t.string :addresses, null: false
-      t.string :building, null: false
-      t.string :phone_number, null: false
+      t.string  :postal_code,   null: false, default: ""
+      t.integer :prefecture_id, null: false, default: ""
+      t.string  :city,          null: false, default: ""
+      t.string  :addresses,     null: false, default: ""
+      t.string  :building,      null: false, default: ""
+      t.string  :phone_number,  null: false, default: ""
 
       t.timestamps
     end
